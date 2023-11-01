255b74f4903c35a09705bc0a202f0efa6649bb35
STD\s20227064 <fujintaran3@gmail.com>
2023-11-01 14:02:26 +0900

    first

---
+++ ClientManagement/Scripts/CreateTableQuery.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SQLite;
+
+namespace ClientManagement.Scripts
+{
+    public class CreateTableQuery
+    {
+        public struct ColumSchema
+        {
+            public enum sqlType
+            {
+                Integer,
+                Text,
+                Real,
+                Blob,
+            }
+
+            public string Name { get; set; }
+            public sqlType Type { get; set; }
+
+            public ColumSchema(string name, sqlType type)
+            {
+                Name = name;
+                Type = type;
+            }
+
+        }
+
+        public CreateTableQuery()
+        {
+
+        }
+    }
+}
